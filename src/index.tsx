158a62570c43089ee449cb4b32197014867b8e5e
Harol johany Zuluaga López <harolz87@gmail.com>
2021-10-15 11:32:12 -0500

    add 404.html for The pade works in github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>, document.getElementById('root'),
+);
