e0e20054130a79e0ed428bc1e856589ee4ccd024
Harol johany Zuluaga López <harolz87@gmail.com>
2021-10-15 13:46:00 -0500

    add dummypage

--- src/pages/dummy/dummy.page.tsx
+++ src/pages/dummy/dummy.page.tsx
@@ -1,7 +1,22 @@
+import Box from '@mui/system/Box';
+import Container from '@mui/material/Container';
 import React from 'react';
 
 export const Dummy = (): JSX.Element => (
-  <div>
-    dummy
-  </div>
+  <>
+    <h2>Dummy</h2>
+    <Container>
+      <Box>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus ligula mi, id ultrices urna lacinia vitae. Mauris sit amet odio nec elit tincidunt varius. Nullam nec ligula id risus lacinia tincidunt. Vivamus in sollicitudin dui, sed molestie tortor. Suspendisse laoreet in sapien non ultricies. Vestibulum leo orci, fermentum sed aliquam quis, euismod et mi. Pellentesque eu mollis sem.
+
+        Nulla sed massa eget purus laoreet tincidunt non non felis. Aliquam imperdiet eu magna eget tempus. Vestibulum massa magna, ullamcorper in tristique eu, consequat et tellus. Praesent fringilla, ipsum sit amet facilisis cursus, quam orci convallis arcu, id posuere nisl dolor vel lacus. In risus urna, porttitor id mattis et, mollis sed tortor. Nulla urna ligula, faucibus sit amet maximus elementum, imperdiet in leo. Sed metus purus, dignissim ac condimentum sit amet, malesuada ac leo. Nam suscipit cursus libero, ut porta tellus blandit nec. Aliquam erat volutpat. Maecenas quis lorem mauris.
+
+        Phasellus mattis mollis congue. Vivamus luctus tortor nisi, a tincidunt arcu commodo eu. Nam convallis aliquet felis, non sagittis nisi dictum vel. Vestibulum a quam lectus. Donec ut tincidunt sapien. Vivamus blandit eros sed mi dapibus malesuada. Maecenas convallis eu risus sed mattis. Aenean rutrum est risus, tempus gravida sapien cursus vitae. Duis vestibulum leo quis ligula tempor, imperdiet consequat enim euismod. Donec in dictum lorem. Integer laoreet nec ante ut malesuada. Donec finibus congue sollicitudin. Sed interdum ultrices posuere. Pellentesque eleifend fermentum nulla, in euismod nisl facilisis sed. Proin dapibus quis ante sit amet condimentum.
+
+        Sed risus diam, vulputate at auctor sit amet, finibus a dolor. Aenean ultrices faucibus nisi varius fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor euismod velit, id efficitur nibh pellentesque vel. Aliquam dignissim purus magna, at ultrices libero mattis quis. Morbi vitae hendrerit mauris. Duis ac nisl vitae massa laoreet ullamcorper nec ac urna. Nunc in risus dolor.
+
+        Curabitur nunc urna, placerat ut accumsan quis, mollis eleifend lectus. Maecenas sodales diam vel ex pharetra faucibus. Sed vitae nulla nunc. Quisque tincidunt eu urna a condimentum. Aliquam a lacinia purus. Cras condimentum ornare ultrices. Vestibulum sagittis dictum venenatis. Nulla dolor leo, aliquet quis pulvinar sit amet, pulvinar quis nisi.
+      </Box>
+    </Container>
+  </>
 );
